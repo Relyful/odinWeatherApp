f1c9dbe3e1989d8a989b5b9c1994675ce2227e62
Relyful <13381270+Relyful@users.noreply.github.com>
2024-04-30 22:57:48 +0200

    Fetch from API into console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 
 console.log('This is a test.');
+
+async function getWeather(city) {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=9ebd06ffaada483cb89191451242404&q=london&q=${city}`, { mode: 'cors' });
+  console.log(response);
+  const object  = await response.json();
+  console.log(object.current);
+}
+
+getWeather('bratislava');
