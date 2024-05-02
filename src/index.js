50ee8a6f883498af03f171f58933dcfddce71ea0
Relyful <13381270+Relyful@users.noreply.github.com>
2024-05-02 19:54:57 +0200

    Add CSS reset

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css';
 console.log('This is a test.');
 
 async function getWeather(city) {
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=9ebd06ffaada483cb89191451242404&q=london&q=${city}`, { mode: 'cors' });
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=9ebd06ffaada483cb89191451242404&q=london&q=${city}`, { mode: 'cors' });
   console.log(response);
   const object  = await response.json();
   console.log(object.current);
