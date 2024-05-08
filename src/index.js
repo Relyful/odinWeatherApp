9de18acda3d8e3b181297ea790a715ac1b99d4b5
Relyful <13381270+Relyful@users.noreply.github.com>
2024-05-08 19:19:01 +0200

    Style the query form

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import getWeatherData from './modules/workData';
 import { unitChangeUI, currentLocation } from './modules/changeUnit';
 
 const input = document.querySelector('#city');
+const button = document.querySelector('#getWeather');
+const form = document.querySelector('#query');
 const fahrenheit = document.querySelector('.fahrenheit');
 
-getWeatherData('Bratislava');
-input.addEventListener('change', () => {
+function submit() {
   getWeatherData(input.value);
   currentLocation.current = input.value;
-});
+  form.reset();
+}
+
+getWeatherData('Bratislava');
+button.addEventListener('click', submit);
+input.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) {
+    submit();
+  }
+})
 
 fahrenheit.classList.add('idleUnit');
 unitChangeUI();
